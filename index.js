a2120bcbca78ee92f4c85175a67396ac43ed8c63
MCHDF <j506661@gmail.com>
2021-08-13 08:22:12 +0900

    2021년 08월 13일 점검 시작

--- index.js
+++ index.js
@@ -29,13 +29,14 @@ bot.on('message', async message => {
     if (message.content === '!점검') {
         message.delete()
         let embed = new Discord.MessageEmbed()
-            .setTitle('Maintenance')
+            .setTitle('Inspection')
             .setColor('RED')
             .setDescription('현재 점검중이에요..자세한 사항은아래에서 확인해주세요!')
-            .addField('[ 사유 ]', '```봇 서버 운영체제, 데이터베이스 등 전반적인 시스템 점검```')
-            .addField('[ 기간 ]', '```3시간 (~ XX:XX)\n(사정에 따라 연장 혹은 조기종료 될 수 있습니다.)```')
+            .addField('[ 사유 ]', '```Node.js & npm 업데이트 및 라즈베리파이 점검```')
+            .addField('[ 기간 ]', '```- 2021년 08월 13일 오후 12시까지```')
             .setTimestamp()
             .setThumbnail(bot.user.displayAvatarURL())
+            .setFooter(bot.fetchApplication().owner.tag)
         message.channel.send(embed);
         return;
     }
